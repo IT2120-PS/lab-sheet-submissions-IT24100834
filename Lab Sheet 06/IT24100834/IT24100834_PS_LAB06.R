29ca206f768b2a69ad6449c91c4a6742caad7a65
IT24100834 <IT24100834@my.sliit.lk>
2025-09-16 23:25:43 +0530

    PS LAB 06

---
+++ Lab Sheet 06/IT24100834/IT24100834_PS_LAB06.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\user\\Desktop\\IT24100834")
+getwd()
+
+
+#Exercise
+
+#1)
+punif(25, min = 0, max = 40, lower.tail = TRUE) - punif(10, min = 0, max = 40, lower.tail = TRUE)
+
+#2)
+pexp(2, rate = 1/3, lower.tail = TRUE)
+
+#3)
+#Part 1
+1 - pnorm(130, mean = 100, sd = 15, lower.tail = TRUE)
+
+#Part 2
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
